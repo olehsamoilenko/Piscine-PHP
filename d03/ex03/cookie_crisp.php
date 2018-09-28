34fd040f405c65a63263003011dab52b84ccd2c9
Oleh SAMOILENKO <osamoile@e1r6p7.unit.ua>
2018-09-28 15:04:09 +0300

    d03, d04

--- d03/ex03/cookie_crisp.php
+++ d03/ex03/cookie_crisp.php
@@ -1,5 +1,28 @@
 <?php
 	foreach ($_GET as $key => $elem) {
-		echo "$key: $elem\n";
+		switch ($key) {
+			case "action":
+				$action = $elem;
+				break;
+			case "name":
+				$name = $elem;
+				break;
+			case "value":
+				$value = $elem;
+				break;
+		}
+	}
+	switch ($action) {
+		case "set":
+			setcookie($name, $value, time() + 3600 * 24);
+			break;
+		case "get":
+			if ($_COOKIE[$name] != null) {
+				echo "$_COOKIE[$name]\n";
+			}
+			break;
+		case "del":
+			setcookie($name, null, 0);
+			break;
 	}
 ?>
